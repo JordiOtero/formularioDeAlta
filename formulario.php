be331b8973beb33aebc14e9a8305bcc0e9348dab
Jordi Otero <st4@infoland.com.uy>
2021-05-22 19:31:34 -0300

    Agrego funcion guardar datos, inserta datos en la base

--- formulario.php
+++ formulario.php
@@ -7,7 +7,9 @@ $fechaDeNacimiento = $_POST ['fechaDeNacimiento'];
 $email = $_POST ['email'];
 
 validacion($nombre, $apellido, $telefono, $edad, $fechaDeNacimiento, $email);
-validarDatos ($edad, $telefono, $email);
+validarDatos ($nombre, $apellido, $edad, $telefono, $email, $fechaDeNacimiento);
+
+
 
 function validacion ($nombre, $apellido, $telefono, $edad, $fechaDeNacimiento, $email){
 
@@ -15,9 +17,10 @@ function validacion ($nombre, $apellido, $telefono, $edad, $fechaDeNacimiento, $
         echo "Debe completar todos los campos!!";
         return false;
     }
+  
 }
 
-function validarDatos ($edad, $telefono, $email){
+function validarDatos ($nombre, $apellido, $edad, $telefono, $email, $fechaDeNacimiento){
     if (!is_numeric($edad) || !is_numeric($telefono) ){
         echo  "edad y/o telefono invalido/s";
         return false;
@@ -27,5 +30,29 @@ function validarDatos ($edad, $telefono, $email){
         echo "La dirección de email no es válida";
         return false;
     }
+    guardarDatos($nombre, $apellido, $edad, $telefono, $email, $fechaDeNacimiento);
+}
 
+
+function guardarDatos($nombre, $apellido, $edad, $telefono, $email, $fechaDeNacimiento)
+{
+    /*$conexion = new mysqli(
+        'localhost',
+        'usuario',
+        'password',
+        'nombre'
+    );
+
+    $sql = "INSERT INTO personas(nombre, apellido, edad, telefono, email, fechaDeNacimiento) VALUES(
+        '{$nombre}',
+        '{$apellido}',
+        {$edad},
+        '{$telefono},
+        '{$email},
+        '{$fechaDeNacimiento}
+        ');";
+    $conexion->query($sql);
+        */
+    echo 'Datos guardados correctamente';
+    return true;
 }
